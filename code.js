d3be1f12f99b49ba53d37f024a75eb28c7174ec2
Mateusz Meler <mateuszmeler@icloud.com>
2022-05-17 22:10:19 +0200

    adding suqares to js

--- code.js
+++ code.js
@@ -1,1 +1,13 @@
 // alert("działa");
+
+let button = document.querySelector(".button--js");
+let counter = 0;
+
+button = document.addEventListener("click", () => {
+  const divElement = document.createElement("div");
+  divElement.textContent = `${counter}`;
+  divElement.className = "square";
+  document.querySelector("div").appendChild(divElement);
+  counter++;
+  console.log("kliknąłem");
+});
